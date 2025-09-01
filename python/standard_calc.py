cbf51746f7cfaf3dfeed3818dbec494a3c62afe9
digcheese <jeffxue269@gmail.com>
2025-08-31 17:38:36 -0700

    linted

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -11,7 +11,7 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return (angle + 180) % 360 - 180 
+    return (angle + 180) % 360 - 180
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
@@ -31,10 +31,9 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     """
 
     # finds the bounded difference between angles
-    first_second_difference: float = (first_angle - second_angle + 180) % 360 - 180
-    middle_first_difference: float = (first_angle - middle_angle + 180) % 360 - 180
-    middle_second_difference: float = (second_angle - middle_angle + 180) % 360 - 180
-
+    first_second_difference: float = abs((first_angle - second_angle + 180) % 360 - 180)
+    middle_first_difference: float = abs((first_angle - middle_angle + 180) % 360 - 180)
+    middle_second_difference: float = abs((second_angle - middle_angle + 180) % 360 - 180)
 
     # the magnitude of angle between first and second must be greater than the magnitude betwen middle and second and first.
-    return abs(first_second_difference) > abs(middle_second_difference) and abs(first_second_difference) > abs(middle_first_difference)
+    return first_second_difference > middle_second_difference and first_second_difference > middle_first_difference
