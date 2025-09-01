ee304cefef4fcf2052cfa751b47180ea70dd74aa
digcheese <jeffxue269@gmail.com>
2025-08-31 17:33:16 -0700

    quiz completed

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -11,7 +11,7 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return 0
+    return (angle + 180) % 360 - 180 
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
@@ -29,4 +29,12 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
-    return True
+
+    # finds the bounded difference between angles
+    first_second_difference: float = (first_angle - second_angle + 180) % 360 - 180
+    middle_first_difference: float = (first_angle - middle_angle + 180) % 360 - 180
+    middle_second_difference: float = (second_angle - middle_angle + 180) % 360 - 180
+
+
+    # the magnitude of angle between first and second must be greater than the magnitude betwen middle and second and first.
+    return abs(first_second_difference) > abs(middle_second_difference) and abs(first_second_difference) > abs(middle_first_difference)
--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -6,6 +6,10 @@ from standard_calc import bound_to_180, is_angle_between
 
 def test_bound_basic1():
     assert bound_to_180(0) == 0
+    assert bound_to_180(180) == -180
+    assert bound_to_180(-180) == -180
+    assert bound_to_180(135) == 135.0
+    assert bound_to_180(200) == -160.0
 
 
 """ Tests for is_angle_between() """
@@ -13,3 +17,9 @@ def test_bound_basic1():
 
 def test_between_basic1():
     assert is_angle_between(0, 1, 2)
+    assert is_angle_between(0, 45, 90)
+    assert not is_angle_between(45, 90, 270)
+    assert is_angle_between(100, 110, -170)
+    assert not is_angle_between(-10, 30, 320)
+    assert is_angle_between(2, 1, 0)
+    assert is_angle_between(-90, 179, -180)
